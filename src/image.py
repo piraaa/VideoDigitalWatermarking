e2f33473b634cbd9ddc3b3cbbd1825cccc2c06dc
pira <pira.y1121@gmail.com>
2017-07-31 14:39:07 +1000

    create new functions in image.py
    readImage,writeImage,printImage

--- src/image.py
+++ src/image.py
@@ -4,3 +4,22 @@
 #
 
 #coding: utf-8
+
+import numpy as np
+import cv2
+
+def readImage(filename):
+	#imreadのflags flags>0(cv2.IMREAD_COLOR):3ChannelColors,flags=0(cv2.IMREAD_GRAYSCALE):GrayScale,flags<0(cv2.IMREAD_UNCHANGED):Original
+	#白黒画像でも強制的にRGBで扱う．
+	img = cv2.imread(filename, 1)
+	print('Read ' + filename + '.')
+	return img
+
+def writeImage(filename, img):
+	cv2.imwrite(filename, img)
+	print('Write '+filename+'.')
+
+def printImage(img):
+	cv2.imshow('Image', img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
