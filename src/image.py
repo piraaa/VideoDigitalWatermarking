244f65eec18d27a0825d9d7b7908c6b0e50c7007
pira <pira.y1121@gmail.com>
2017-08-02 12:59:52 +1000

    readImage → readGrayImage & readColorImage
    readImage function → readGrayImage & readColorImage functions.
    Add docstring for some functions.

--- src/image.py
+++ src/image.py
@@ -4,14 +4,31 @@
 #
 
 #coding: utf-8
+u"""For image processing."""
 
 import numpy as np
 import cv2
 
-def readImage(filename):
-	#imreadのflags flags>0(cv2.IMREAD_COLOR):3ChannelColors,flags=0(cv2.IMREAD_GRAYSCALE):GrayScale,flags<0(cv2.IMREAD_UNCHANGED):Original
-	#白黒画像でも強制的にRGBで扱う．
-	img = cv2.imread(filename, 1)
+def readGrayImage(filename):
+	u"""Read grayscale image.
+	@param  filename:filename
+	@return img     :2 dimension np.ndarray[Height][Width]
+	"""
+	#imread flags=0(cv2.IMREAD_GRAYSCALE):GrayScale
+	img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+	print('Read "' + filename + '".')
+	return img
+
+def readColorImage(filename):
+	u"""Read color image.
+	[notice] Grayscale images are treated as RGB image. 
+	         (ex. if pixel value is 100, it's treated [100][100][100] RGB image.)
+	@param  filename:filename
+	@return img     :3 dimension np.ndarray[Height][Width][BGR]
+	"""
+	#imread flags>0(cv2.IMREAD_COLOR):3ChannelColors
+	#白黒画像でも強制的にRGBで扱ってしまう．
+	img = cv2.imread(filename, cv2.IMREAD_COLOR)
 	print('Read "' + filename + '".')
 	return img
 
@@ -53,3 +70,17 @@ def ycc2rgb(img):
 			rgb_data[i][j][1] = img[i][j][0] - 0.344*img[i][j][1] - 0.714*img[i][j][2] #G
 			rgb_data[i][j][2] = img[i][j][0] + 1.402*img[i][j][2] #R
 	return rgb_data
+
+def get_y(img):
+	u"""Get only Y from YCC image.
+	@param  img    :3 dimension np.ndarray[Height][Width][YCC]
+	@return y_data :2 dimension np.ndarray[Height][Width]. Including only Y.
+	"""
+	height = img.shape[0]
+	width  = img.shape[1]
+	y_data = np.empty([height,width])
+	for i in np.arange(height):
+		for j in np.arange(width):
+			y_data[i][j] = img[i][j][0]
+	return y_data
+
