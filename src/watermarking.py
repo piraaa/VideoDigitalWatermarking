7642f223a91e69b18b29015d6bd6a7606ba6a476
pira <pira.y1121@gmail.com>
2017-08-03 16:10:06 +1000

    create embed_lsb
    Created new function named embed_lsb. This function embeds secret informations by changing LSB.
    
    Created _image2vector and _vector2image for embed_lsb.

--- src/watermarking.py
+++ src/watermarking.py
@@ -6,8 +6,44 @@
 #coding: utf-8
 u"""For image watermarking."""
 
+import sys
+import numpy as np
+
 TIME_DOMAIN = 1
 DCT_DOMAIN  = 0
+
+def embed_lsb(cover, secret, interval=0):
+	u"""Embed secret informations by changing LSB.
+ 	@param  cover   :cover data (2 dimension np.ndarray)
+ 	@param  secret  :0 or 1 secret information (list)
+ 	@param  interval:enbed interval
+ 	@return stego   :srego data (2 dimension np.ndarray)
+	"""
+	height = cover.shape[0]
+	width  = cover.shape[1]
+	es_length = len(secret) * (1+interval) #Embeded Sequense length
+
+	if height*width < es_length:
+		print('Secret information is over the limit of embeded.')
+		print('Please review the cover image, secret information or interval.')
+		sys.exit()
+
+	#create embeded sequense
+	es = np.zeros(es_length)
+	for i, secret_bit in enumerate(secret):
+			es[i*(interval+1)] = secret_bit
+
+	cover = _image2vrctor(cover)
+	stego = cover
+
+	for i, secret_bit in enumerate(es):
+		stego[i] = _add_lsb(cover[i], secret_bit)
+
+	stego = _vector2image(stego, height, width)
+
+	return stego
+
+#def extract_lsb():
 
 def _add_lsb(cover, secret, mode=0):
 	u"""Embed 1 bit secret information by changing LSB.
@@ -43,3 +79,27 @@ def _check_lsb(data):
 	elif data%2 == 1:
 		return 1
 
+def _image2vrctor(img):
+	u"""Convert image to vector.
+ 	@param  img   :2 dimension image data
+ 	@return vector:1 dimension image data
+	"""
+	height = img.shape[0]
+	width  = img.shape[1]
+	len = height * width
+	vector = np.empty(len)
+	for i in np.arange(height):
+		for j in np.arange(width):
+			vector[i*height+j] = img[i][j]
+	return vector
+
+def _vector2image(vector, height, width):
+	u"""Convert vector to image.
+ 	@param  vector:1 dimension image data
+ 	@return image :2 dimension image data
+	"""
+	image = np.empty([height, width])
+	for i in np.arange(height):
+		for j in np.arange(width):
+			image[i][j] = vector[i*height+j]
+	return image
