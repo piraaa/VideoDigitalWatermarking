e0d654f7f64a1090f107b17dd8ca4fa6a487e2b9
pira <pira.y1121@gmail.com>
2017-08-14 15:13:13 +1000

    delete unnecessary functions

--- src/secret.py
+++ src/secret.py
@@ -16,18 +16,3 @@ def generateSecret(n):
 	"""
 	secret = np.random.randint(2, size =n)
 	return secret
-
-def str2bi(secret_str):
-	u"""Convert string to binary.
-	@param  secret_str : string secret information
-	@return secret_bi : binary secret information 
-	"""
-	pass
-
-
-def bi2str(secret_bi):
-	u"""Convert binary to string.
-	@param  secret_bi : binary secret information
-	@return secret_str : string secret information 
-	"""
-	pass
