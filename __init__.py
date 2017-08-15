0439b76bf56d206e13fa75478d8ed1f467a53c93
pira <pira.y1121@gmail.com>
2017-08-15 12:52:09 +1000

    update __init__
    Add m-sequence.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from VideoDigitalWatermarking.src.ber import *
 from VideoDigitalWatermarking.src.dct import *
 from VideoDigitalWatermarking.src.image import *
+from VideoDigitalWatermarking.secret.msequence import *
 from VideoDigitalWatermarking.src.psnr import *
 from VideoDigitalWatermarking.src.secret import *
 from VideoDigitalWatermarking.src.video import *
