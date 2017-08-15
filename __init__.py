74734a549b213ff09a5905bce01f36c26f5c52b1
pira <pira.y1121@gmail.com>
2017-08-15 15:22:28 +1000

    add correlate

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from VideoDigitalWatermarking.src.ber import *
+from VideoDigitalWatermarking.src.correlate import *
 from VideoDigitalWatermarking.src.dct import *
 from VideoDigitalWatermarking.src.image import *
 from VideoDigitalWatermarking.src.msequence import *
