3423fe0067b2e563787628833f9f5fc04fd844a8
pira <pira.y1121@gmail.com>
2017-08-15 19:42:02 +1000

    create generateM

--- src/msequence.py
+++ src/msequence.py
@@ -2,3 +2,40 @@
 # msequence.py
 # Created by pira on 2017/07/28.
 #
+
+#coding: utf-8
+u"""For M-Sequence."""
+
+import numpy as np
+
+def generateM(N):
+	u"""Create M-Sequence.
+ 	@param  N : length 2**N-1
+ 	@return m : M-Sequence
+	"""
+
+	p = pow(2, N)
+	m = [0] * (p-1)
+
+	for i in np.arange(1,p,2): 
+		f = p^i
+		a = p
+		#i = int()
+		for j in np.arange(N, p):
+			if (a&p) == p:
+				a ^= f
+			if a == 1:
+				break
+			a <<= 1
+		if j == p-1:
+			init = 1
+			lfsr = init & (p-1)
+			f >>= 1
+			for k in np.arange(0, p-1):
+				lfsr = (lfsr>>1)^(-(int)(lfsr&1) & f)
+				m[k] = (lfsr&1) * 2-1
+			return m
+
+#test
+#m = generateM(3)
+#print(m)
