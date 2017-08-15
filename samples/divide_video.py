b0c823506b7ba514df8f5f67c786a21a1bd08db7
pira <pira.y1121@gmail.com>
2017-08-15 10:49:21 +1000

    create video divide sample

---
+++ samples/divide_video.py
@@ -0,0 +1,12 @@
+# 
+# divide_video.py
+# Created by pira on 2017/08/07.
+#
+
+#coding: utf-8
+
+from VideoDigitalWatermarking import *
+import numpy as np
+
+filename  = 'test.mp4'
+video2image(filename, n=5)
